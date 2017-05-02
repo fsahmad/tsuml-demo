79f14d19332be478438a603019d7d498b8bf4fb6
fsamer <fsamer@springwireless.com>
2017-05-01 20:52:10 -0500

    Added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import {
+  createStore,
+  compose,
+} from 'redux';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import './index.css';
 
+// tslint:disable-next-line:no-string-literal
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  // tslint:disable-next-line:no-any
+  (state: any) => state,
+  composeEnhancers(),
+);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
